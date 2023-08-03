df618a5836b2c1472a7e59f8d095785ad447b0ed
Kim Bramwell Mazzarella <kimbrammazz@gmail.com>
2023-08-03 17:39:11 +1000

    basic page up and running

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const { v4: uuid } = require("uuid");
+
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
 
 // Our fake database:
 let todo = [
@@ -10,8 +15,8 @@ let todo = [
 	},
 	{
 		id: uuid(),
-		username: "Renew passport",
-		comment: "Fill in online application and make appointment",
+		task: "Renew passport",
+		description: "Fill in online application and make appointment",
 	},
 ];
 
@@ -23,6 +28,11 @@ let todo = [
 // 	/todo/:id/edit - GET - form to edit one specific todo
 // 	/todo/:id - PATCH - updates specific todo on "server"
 // 	/todo/:id - DELETE - deletes specific todo on "server"
+
+//display all todos
+app.get("/todo", (req, res) => {
+	res.render("todo/index", { todo });
+});
 
 app.listen(3000, () => {
 	console.log("listening on port 3000");
