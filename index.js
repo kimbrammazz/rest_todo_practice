816cc667cf91b2969cf0a151d569556790810ec5
Kim Bramwell Mazzarella <kimbrammazz@gmail.com>
2023-08-04 17:11:26 +1000

    add 'new' route

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const path = require("path");
 const { v4: uuid } = require("uuid");
 
+// to parse the body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
@@ -22,7 +26,7 @@ let todo = [
 
 // Routes to configure
 // 	/todo - GET - display all todos
-// 	/todo/new - GET - form to create new todo
+// 	/todo/add - GET - form to create new todo
 // 	/todo - POST - creates new todo on 'server'
 // 	/todo/:id - GET - details for one specific todo
 // 	/todo/:id/edit - GET - form to edit one specific todo
@@ -32,6 +36,17 @@ let todo = [
 //display all todos
 app.get("/todo", (req, res) => {
 	res.render("todo/index", { todo });
+});
+
+app.get("/todo/add", (req, res) => {
+	res.render("todo/add");
+});
+
+app.post("/todo", (req, res) => {
+	// console.log(req.body);
+	const { task, description } = req.body;
+	todo.push({ task, description, id: uuid() });
+	res.redirect("/todo");
 });
 
 app.listen(3000, () => {
