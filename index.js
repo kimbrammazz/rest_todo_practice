63ffa446082abf8d0086fbb9090b592222c86136
Kim Bramwell Mazzarella <kimbrammazz@gmail.com>
2023-09-08 14:53:15 +1000

    finished update, add delete route

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const methodOverride = require("method-override");
 const { v4: uuid } = require("uuid");
 
 // to parse the body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// to use method override
+app.use(methodOverride("_method"));
 
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
@@ -43,14 +46,6 @@ app.get("/todo/add", (req, res) => {
 	res.render("todo/add");
 });
 
-// form to add new todo
-app.post("/todo", (req, res) => {
-	// console.log(req.body);
-	const { task, description } = req.body;
-	todo.push({ task, description, id: uuid() });
-	res.redirect("/todo");
-});
-
 // show details of task
 app.get("/todo/:id", (req, res) => {
 	const { id } = req.params;
@@ -63,6 +58,32 @@ app.get("/todo/:id/edit", (req, res) => {
 	const { id } = req.params;
 	const currentTask = todo.find((t) => t.id === id);
 	res.render("todo/edit", { currentTask });
+});
+
+// to update the details
+app.patch("/todo/:id", (req, res) => {
+	const { id } = req.params;
+	const updatedTask = req.body.task;
+	const updatedDescription = req.body.description;
+	const currentTask = todo.find((t) => t.id === id);
+	currentTask.task = updatedTask;
+	currentTask.description = updatedDescription;
+	res.redirect("/todo");
+});
+
+app.delete("/todo/:id", (req, res) => {
+	const { id } = req.params;
+	// loop through array, and put into new array where todo.id is not equal to the id in the path
+	todo = todo.filter((t) => t.id !== id);
+	res.redirect("/todo");
+});
+
+// form to add new todo
+app.post("/todo", (req, res) => {
+	// console.log(req.body);
+	const { task, description } = req.body;
+	todo.push({ task, description, id: uuid() });
+	res.redirect("/todo");
 });
 
 app.listen(3000, () => {
